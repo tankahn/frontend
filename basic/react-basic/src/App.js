463a59abff7fa900af1ac49ab7dba7bafaf386e4
tankahn <tankahn@naver.com>
2023-01-26 21:42:33 +0900

    todo

--- basic/react-basic/src/App.js
+++ basic/react-basic/src/App.js
@@ -805,23 +805,551 @@ export default App;
 Q. 구독하기 버튼 만들기 
 */
 
+// function App() {
+
+//   /*
+//    const [subscribed, setSubscribed] = useState(false);
+//   */
+
+//    const [gu, noGu] = useState(false);
+//     console.log(gu)
+
+//   return (
+//         <>
+//         <h1>구독버튼</h1>
+
+//         <button onClick={() => noGu(!gu)}>
+//          {gu ? '구독취소' : '구독하기'}
+//          </button>
+//         </>
+//       )
+
+// }
+
+// // function App() {
+// //   const [count, setCount] = useState(0);
+
+// //   return (
+// //     <>
+// //       <h1>count: {count}</h1>
+
+// //       <Article 
+// //         count={count}
+// //         setCount={setCount} 
+// //       />  
+// //     </>
+// //   )
+// // }
+
+// // function Article(props) {
+// //   return (
+// //     <>
+// //       <button onClick={() => props.setCount(props.count + 1)}>
+// //         Add
+// //       </button>
+// //     </>  
+// //   )
+// // }
+
+
+// /*
+//   리액트 라우터
+//   1 리액트 라우터란?
+//     라우터: 요청(특히 url)에 적합한 작업을 수행하는 것
+//   2 라우터 기본
+//   3 인증이 적용된 라우터
+// */
+
+
+// // function Home() {
+// //   return <h1>Home</h1>
+// // }
+
+// // function About() {
+// //   return <h1>About</h1>
+// // }
+
+// // function Posts() {
+// //   return (
+// //     <>
+// //       <h1>Posts</h1>
+// //       <ul>
+// //         <li>
+// //           <Link to="/post/p1">Post 1</Link>
+// //         </li>
+// //         <li>
+// //           <Link to="/post/p2">Post 2</Link>
+// //         </li>
+// //       </ul>
+// //     </>  
+// //   )
+// // }
+
+// // function Post() {
+// //   /*
+// //     useParams
+// //     url로 전달된 파라미터에 접근하게 한다
+// //   */
+// //   const params = useParams();
+// //   const postId = params.postId;
+
+// //   // postId를 서버에 전송한다
+// //   console.log(params);
+
+// //   return (
+// //     <>
+// //       <h1>Title</h1>
+// //       <p>{postId}</p>
+// //     </>
+// //   )
+// // }
+
+// // function NotFound() {
+// //   return <h1>404 NotFound</h1>
+// // }
+
+// // function App() {
+// //   return (
+// //     <Router>
+// //       <nav>
+// //         <ul> 
+// //           <li>
+// //             <Link to="/">Home</Link>
+// //           </li>
+// //           <li>
+// //             <Link to="/about">About</Link>
+// //           </li>
+// //           <li>
+// //             <Link to="/posts">Posts</Link>
+// //           </li>
+// //         </ul>
+// //       </nav>
+
+// //       <Routes>
+// //         <Route path="/" element={<Home />} />
+// //         <Route path="about" element={<About />} />
+// //         <Route path="posts" element={<Posts />} />
+// //         <Route path="post/:postId" element={<Post />} />
+// //         <Route path="*" element={<NotFound />} />
+// //       </Routes>
+// //     </Router>
+// //   )
+// // }
+
+
+// // 3 인증이 적용된 라우터
+
+// // function App() {
+// //   return (
+// //     <Router>
+// //       <AuthProvider>
+// //         <Routes>
+// //           <Route path="/" element={<Layout />}>
+// //             <Route index element={<Home />} />
+// //             <Route path="posts" element={<Posts />} />
+// //             <Route path="post/:postId" element={
+// //               <AuthRequired>
+// //                 <Post />
+// //               </AuthRequired>
+// //             } />
+// //             <Route path="*" element={<NotFound />} />
+// //           </Route>
+// //         </Routes>
+// //       </AuthProvider>
+// //     </Router>
+// //   )
+// // }
+
+// // const AuthContext = createContext();
+
+// // function AuthProvider(props) {
+// //   const [user, setUser] = useState(null);
+
+// //   // 로그인
+// //   function signIn(username) {
+// //     setUser(username);
+// //   }
+
+// //   // 로그아웃
+// //   function signOut() {
+// //     setUser(null);
+// //   }
+
+// //   const value = {user, signIn, signOut};
+
+// //   return (
+// //     <AuthContext.Provider value={value}>
+// //       {props.children}
+// //     </AuthContext.Provider>
+// //   )
+// // }
+
+// // function Layout() {
+// //   const auth = useContext(AuthContext);
+
+// //   return (
+// //     <>
+// //       <nav>
+// //         <ul>
+// //           <li>
+// //             <Link to="/">Home</Link>
+// //           </li>
+// //           <li>
+// //             <Link to="/posts">Posts</Link>
+// //           </li>
+// //         </ul>
+// //       </nav>
+
+// //       {auth.user ? (
+// //         <p>
+// //           Hi, {auth.user} {" "}
+// //           <button onClick={auth.signOut}>
+// //             Logout
+// //           </button>
+// //         </p>
+// //       ):(
+// //         <p>
+// //           Not logged in
+// //         </p>
+// //       )}
+
+// //       <Outlet />
+// //     </>
+// //   )
+// // }
+
+// // function Home() {
+// //   return <h1>Home</h1>
+// // }
+
+// // function Posts() {
+// //   return (
+// //     <>
+// //       <h1>Posts</h1>
+// //       <ul>
+// //         <li>
+// //           <Link to="/post/p1">Post 1</Link>
+// //         </li>
+// //         <li>
+// //           <Link to="/post/p2">Post 2</Link>
+// //         </li>
+// //       </ul>
+// //     </>  
+// //   )
+// // }
+
+// // function AuthRequired(props) {
+// //   const auth = useContext(AuthContext);
+
+// //   console.log(auth);
+
+// //   function handleSubmit(e) {
+// //     e.preventDefault();
+
+// //     const formData = new FormData(e.target);
+
+// //     auth.signIn(formData.get('username'));
+// //   }
+
+// //   if (!auth.user) {
+// //     return (
+// //       <form onSubmit={handleSubmit}>
+// //         <h1>Login</h1>
+// //         <input type="text" name="username" required />
+// //         <button type="submit">Login</button>
+// //       </form>
+// //     )
+// //   }
+
+// //   return props.children;
+// // }
+
+// // function Post() {
+// //   const params = useParams();
+// //   const postId = params.postId;
+
+// //   return (
+// //     <>
+// //       <h1>Post</h1>
+// //       <p>{postId}</p>
+// //     </>  
+// //   )
+// // }
+
+// // function NotFound() {
+// //   return <h1>404 NotFound</h1>
+// // }
+
+
+// /*
+//  데이터 가져오기
+//  1 데이터 가져오기란?
+//   서버에 데이터를 요청해서 응답받는 것
+//   2 useEffect Hook
+//   3 데이터 가져오기 예시
+// */
+
+
+// // 2 useEffect
+
+// // function App() {
+// //   const [count, setCount] = useState(0);
+
+// //   /*
+
+// //     useEffect
+// //     비동기적으로 작동한다
+
+// //     1 useEffect(effect): effect는 컴포넌트가 실행될 때마다 실행된다
+// //     2 useEffect(effect, []): effect는 컴포넌트의 최초 실행시에만 실행된다
+// //     3 useEffect(effect, [dep]): effect는 컴포넌트의 최초 실행시,
+// //     dependency가 업데이트 될 때마다 실행된다
+
+// //   */
+
+// //   useEffect(() => {
+// //     console.log('lol')
+// //   }, [])
+
+// //   return (
+// //     <>
+// //       <h1>count: {count}</h1>
+// //       <button onClick={() => setCount(count + 1)}>+</button>
+// //     </>
+// //   )
+// // }
+
+
+// // 3 데이터 가져오기 예시
+
+
+// // 서버에 데이터를 요청하는 함수
+// // 서버에서 응답을 주는데 2초가 걸린다고 가정
+// function fetchData() {
+
+//   const DATA = {
+//     username: 'danaka',
+//     image: 'https://image.xportsnews.com/contents/images/upload/article/2022/1206/mb_1670300078707386.jpg',
+//     bio: '안녕하세요 여러붕구, 다나카입니다 ^00^'
+//   }
+
+//   const promise = new Promise((res, rej) => {
+//     setTimeout(() => {
+//       res(DATA)
+//     }, 2000)
+//   })
+
+//   return promise;
+// }
+
+// function App() {
+
+//   const [error, setError] = useState(null);
+//   const [isLoaded, setIsLoaded] = useState(false);
+//   const [profile, setProfile] = useState(null);
+
+//   useEffect(() => {
+//     fetchData()
+//     .then(data => {
+//       // profile 업데이트
+//       setProfile(data)
+//     })
+//     .catch(error => {
+//       setError(error)
+//     })
+//     .finally(() => setIsLoaded(true))
+//   }, [])
+
+//   if (error) {
+//     return <p>failed to fetch profile</p>
+//   }
+
+//   if (!isLoaded) {
+//     return <p>fetching profile...</p>
+//   }
+
+//   return (
+//     <>
+//       <h1>Profile</h1>
+//       <img
+//         src={profile.image}
+//         alt={profile.username}
+//         style={{
+//           width: '100px',
+//           height: '100px',
+//           objectFit: 'cover',
+//           borderRadius: '50%'
+//         }}
+//       />
+//       <h3>{profile.username}</h3>
+//       <p>{profile.bio}</p>
+//     </>  
+//   )
+// }
+
+const initialTasks = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+];
+
+const FILTER_MAP ={
+  All: () => true,
+  Done : task => task.completed,
+  Active : task => !task.completed
+}
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+// Object : 객체의 속성을 문자열 Array로 리턴하는 속성
+console.log(FILTER_MAP)
+
 function App() {
+  const [tasks, setTasks] = useState(initialTasks);
 
-  /*
-   const [subscribed, setSubscribed] = useState(false);
-  */
+  // tasks 추적하기
+  console.log(tasks);
 
-   const [gu, noGu] = useState(false);
-    console.log(gu)
+  function addTask(name) {
+    const newTask = {
+      id: `todo-${Math.random()}`,
+      name, // name : name 일때 뒤 한번만 적어 됨
+      completed: false
+    }
+
+    const updatedTasks = [...tasks, newTask];
+    setTasks(updatedTasks);
+
+    console.log(updatedTasks)
+  }
+
+  function deleteTask(id) {
+    console.log(id);
+
+    const remainingTasks = tasks.filter(task=> task.id !== id)
+    setTasks(remainingTasks)
+  }
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      if (task.id === id) {
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    })
+
+    setTasks(updatedTasks)
+  }
+
+  const filterButtons = FILTER_NAMES.map (name => (
+    <FilterButton
+      key={name}
+      name={name}
+      />
+  ))
+
+  const taskList = tasks.map(task => (
+    <Todo
+      key={task.id}
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      deleteTask={deleteTask}
+      toggleTaskCompleted={toggleTaskCompleted}
+    />
+  ))
 
   return (
-        <>
-        <h1>구독버튼</h1>
-        
-        <button onClick={() => noGu(!gu)}>
-         {gu ? '구독취소' : '구독하기'}
-         </button>
-        </>
-      )
+    <div className="max-w-sm mx-auto mt-8 border p-4">
+      <h1 className="text-2xl text-center mb-4">할일 목록 &#128526; &#127928;</h1>
 
+      <Form addTask={addTask} />
+      <div className = "flex flex-nowrap gap-1 mb-4">
+        {filterButtons}
+        </div>
+
+      <ul>
+        {taskList}
+      </ul>
+    </div>
+  )
+}
+
+function Form(props) {
+  const [name, setName] = useState("");
+
+  // console.log(name);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.addTask(name);
+    setName("");
+
+  }
+
+  return (
+    <form
+      className="mb-4"
+      onSubmit={handleSubmit}
+    >
+      <input
+        type="text"
+        className="border px-2 py-1 w-full mb-2"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        autoComplete="off"
+      />
+      <button
+        type="submit"
+        className="p-1 w-full border disabled:opacity-50 text-blue-500"
+      >
+        Add
+      </button>
+    </form>
+  )
+
+}
+
+function FilterButton(props) {
+  return (
+    <button 
+      className='p-1 w-1/3 border'
+      >
+        {props.name}
+      </button>
+  )
+ }
+
+function Todo(props) {
+  // console.log(props)
+
+  return (
+    <li className="mb-4">
+      <div className="flex mb-2">
+        <label>
+          <input
+            type="checkbox"
+            className="hidden peer"
+            checked={props.completed}
+            onChange={() => props.toggleTaskCompleted(props.id)}
+          />
+          <span className="text-xl peer-checked:line-through">
+            {props.name}
+          </span>
+        </label>
+      </div>
+      <div className="flex flex-nowrap gap-1">
+        <button
+          className="w-1/2 p-1 border"
+        >
+          Edit
+        </button>
+        <button
+          className="w-1/2 p-1 border text-red-500"
+          onClick={() => props.deleteTask(props.id)}
+        >Delete
+        </button>
+      </div>
+
+    </li>
+  )
 }
